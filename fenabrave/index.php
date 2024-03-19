926ee329116e3935d02feba2a3ccab4406234686
carlospss <48728585+carlospss@users.noreply.github.com>
2024-03-19 16:45:51 -0300

    Add files via upload

---
+++ fenabrave/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fenabrave-Bahia</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>home</li>
+				<li>institucional</li>
+				<li>sincodiv</li>
+				<li>notícias</li>
+				<li>convenções coletivas</li>
+				<li>parceiros</li>
+				<li>eventos</li>
+					<!--<li>calendário</li>
+					<li>inscrisões online</li>
+					<li>combate das marcas</li>-->
+				<li>fale conosco</li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<section>
+			<h1>notícias</h1>
+			<article><!--AQUI VAI SER UM PORTAL DE NOTÍCIAS QUE ACREDITO QUE VIRÃO DO BACKEND--></article>
+		</section>
+	</main>
+	<footer>
+		<section>
+
+		<article>Facebook</article>
+
+		<article>
+			Assine nossa Newsletter
+			Receba novidades em seu e-mail.
+			Obrigado por se cadastrar.
+		</article>
+
+		<article>
+			Endereço
+ 			Av. Juracy Magalhães, 66
+			Edf. Tora, Salas 201 e 202
+			Rio Vermelho, Salvador-BA
+
+ 			71 3240-5260
+ 			Seg - Sex 8:00 - 17:00
+		</article>
+
+		<article>
+			Localização
+			<!--AQUI VAI FICAR UM MAPA-->
+		</article>
+		</section>
+	</footer>
+</body>
+</html>
